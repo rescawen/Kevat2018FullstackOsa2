b9fff80bcc9cfd7ecd5c88b61aa48b7fd4d4f428
rescawen <resca.wen@gmail.com>
2018-02-02 10:08:00 +0200

    osa2 harjoitus alkaaaa

---
+++ osa2/src/index.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const notes = [
+  {
+    id: 1,
+    content: 'HTML on helppoa',
+    date: '2017-12-10T17:30:31.098Z',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Selain pystyy suorittamaan vain javascriptiä',
+    date: '2017-12-10T18:39:34.091Z',
+    important: false
+  },
+  {
+    id: 3,
+    content: 'HTTP-protokollan tärkeimmät metodit ovat GET ja POST',
+    date: '2017-12-10T19:20:14.298Z',
+    important: true
+  }
+]
+
+const App = (props) => {
+  const { notes } = props;
+
+  return (
+    <div>
+      <h1>Muistiinpanot</h1>
+      <ul>
+        <li>{notes[0].content}</li>
+        <li>{notes[1].content}</li>
+        <li>{notes[2].content}</li>
+      </ul>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App notes={notes} />,
+  document.getElementById('root')
+)
